541a677090ddad3a66c573ae7a98d0210bb12030
Gregor Zeman <if19b210@technikum-wien.at>
2021-09-20 02:26:01 +0200

    Finished First Tasks

--- StringLibrary.cpp
+++ StringLibrary.cpp
@@ -5,14 +5,22 @@
 
 using namespace std;
 
-string Concatenate(string string1, string string2)
+int getLength(char* msg)
 {
-    //return combined const char strings
-    string sumString("");
-    sumString += string1.c_str();
-    sumString += ' ' + string2.c_str();
-    cout << sumString; //kein richtiger verweis
-    return sumString.c_str();
+    const char* str = msg;
+    int Size = 0;
+    while (str[Size] != '\0') Size++;
+    return Size;
+}
+
+string Concatenate(char* string1, char* string2)
+{
+    char result[100];           // array to hold the result.
+
+    strcpy(result, string1);    // copy string one into the result.
+    strcat(result, string2);    // append string two to the result.
+
+    return result;
 }
 
 
@@ -20,18 +28,22 @@ int main()
 {
     const char* msg = "That's a string here.";
     const char* msg2 = "That's another one.";
-    printf("%s\n", Concatenate(msg, msg2));
+
+    string conString;
+    conString = Concatenate(const_cast<char*>(msg), const_cast<char*>(msg2));
+    cout << conString << endl;
+
+    cout << "Before modifying: " << msg << endl;
+
+    string modString = "Oops, a new string appeared.";
+
+    msg = modString.c_str(); //We are putting the string on a char array with the c_str() function
+    cout << "After modifying: " << msg << endl;
+
+    const char* msgCount = "This sentence has 26 chars";
+
+    cout << "Char count: " << getLength(const_cast<char*>(msgCount));
+
     return 0;
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
